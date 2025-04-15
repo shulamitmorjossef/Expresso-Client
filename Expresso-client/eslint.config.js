94453277fab5ab4e774fdffa37b8ffd598c55020
shulamitmorjossef <shulamitmorjossef@gmail.com>
2025-04-15 16:22:51 +0300

    Add lint

--- Expresso-client/eslint.config.js
+++ Expresso-client/eslint.config.js
@@ -1,10 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules', 'eslint.config.js'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -21,13 +20,17 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      // "no-unused-vars": ["warn"],
+      "linebreak-style": ["warn", "unix"],
+      "semi": ["error", "always"],
+      "quotes": ["error", "single", { "avoidEscape": true }],
+      "eqeqeq": ["warn", "always"],
+      "no-console": ["warn"],
+      "prefer-const": ["warn"],
+      "no-var": ["error"],
+      "arrow-body-style": ["warn", "as-needed"],
+      "prefer-template": ["warn"],
+      "strict": ["error", "never"]
     },
   },
-]
+];
