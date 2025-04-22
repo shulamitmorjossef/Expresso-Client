bf2f5bee75c4957d4b82f3949901ed39a2fd58d3
shulamitmorjossef <shulamitmorjossef@gmail.com>
2025-04-22 21:28:34 +0300

    Add front

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,22 @@ function App() {
   const [password, setPassword] = useState('');
 
 
-  function submitLogin(event) {
-    const userName = event.formData.get('useName');
-    const password = event.formData.get('password');
-    setUseName(userName);
-    setPassword(password);
-    setShouldSubmit(true);
+  // function submitLogin(event) {
+    // const userName = event.formData.get('useName');
+    // const password = event.formData.get('password');
+    // setUseName(userName);
+    // setPassword(password);
+    // setShouldSubmit(true);
+
+    function submitLogin(event) {
+      event.preventDefault(); // חובה כדי למנוע רענון
+      const formData = new FormData(event.target);
+      const userName = formData.get('userName');
+      const password = formData.get('password');
+      setUseName(userName);
+      setPassword(password);
+    }
+    
 
   }
 
@@ -23,8 +33,8 @@ function App() {
 
     <div className='card'>
       <form id = 'login-form' onSubmit={submitLogin}>
-        <input type="text" id = 'userName' />
-        <input type="password" id = 'password'/>
+        <input type="text" name = 'userName' />
+        <input type="password" name = 'password'/>
         <button type='submit'> Submit </button>
       </form>
     </div>
