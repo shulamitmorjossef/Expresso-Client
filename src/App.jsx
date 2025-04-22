1461d0e091069fd291d9b4198be256b98ab5cba0
shulamitmorjossef <shulamitmorjossef@gmail.com>
2025-04-22 21:14:56 +0300

    Add front

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,31 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [useName, setUseName] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  function submitLogin(event) {
+    const userName = event.formData.get('useName');
+    const password = event.formData.get('password');
+    setUseName(userName);
+    setPassword(password);
+    setShouldSubmit(true);
+
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <div className='card'>
+      <form id = 'login-form' onSubmit={submitLogin}>
+        <input type="text" id = 'userName'> enter your user name </input>
+        <input type="password" id = 'password'> enter your password </input>
+        <button type='submit'> Submit </button>
+      </form>
+    </div>
+
     </>
   );
 }
