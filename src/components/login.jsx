9eeccb7f8d75451cbe7f9c3c6ea73bee27cd5d4f
shulamitmorjossef <shulamitmorjossef@gmail.com>
2025-04-23 12:21:12 +0300

    add registretion

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:3000/users', {
+        username,
+        password
+      });
+      alert('✅ משתמש נוצר בהצלחה!');
+    } catch (err) {
+      console.error('❌ שגיאה ביצירת המשתמש:', err);
+      alert('🚫 יצירת המשתמש נכשלה!');
+    }
+  };
+
   return (
     <div>
       <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          שם משתמש:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          סיסמה:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">הירשם</button>
+      </form>
     </div>
+    
   );
 }
